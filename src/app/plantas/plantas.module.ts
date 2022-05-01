36a2bd7e526bd949b346a7c0fe60c76308888bf8
JRIVE16 <juan.sebastian.rivera.abreo@gmail.com>
2022-05-01 11:41:49 -0500

    * Se crea el modulo _plantas_. * Se crea el componente `routing`.

---
+++ src/app/plantas/plantas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlantasRoutingModule } from './plantas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlantasRoutingModule
+  ]
+})
+export class PlantasModule { }
