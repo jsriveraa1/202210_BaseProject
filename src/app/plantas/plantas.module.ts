a847c7761daf42598ce2ad67c6fd16a6c06bcc97
JRIVE16 <juan.sebastian.rivera.abreo@gmail.com>
2022-05-01 15:20:54 -0500

    * Se crea el componente de listar planta que presentará en una tabla de Bootstrap los datos de la planta (nombre común, tipo y clima).

--- src/app/plantas/plantas.module.ts
+++ src/app/plantas/plantas.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlantasRoutingModule } from './plantas-routing.module';
+import { ListarComponent } from './listar/listar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarComponent
+  ],
+  exports: [
+    ListarComponent
+  ],
   imports: [
     CommonModule,
     PlantasRoutingModule
